8ba12556b2e39664c6672776409c039c33d8ee79
Torin Lahiru <torinlahiru@gmail.com>
2025-05-07 19:13:09 +0530

    trangle_1 created

---
+++ Simple-Programs/Patterns/trangle_1.py
@@ -0,0 +1,21 @@
+
+n = 5
+
+for row in range(1,n+1):
+    for col in range(row):
+        print("*" ,end=' ')
+    print()
+
+
+
+
+
+
+
+# Output 
+
+#  *
+#  * *
+#  * * *
+#  * * * *
+#  * * * * *
