071882cb1cff5031fe6e768b86a62fc709fc5e51
Torin Lahiru <torinlahiru@gmail.com>
2025-05-07 19:08:34 +0530

    square pattern was created

---
+++ Simple-Programs/Patterns/square.py
@@ -0,0 +1,15 @@
+n = 5
+
+for row in range(n):
+    for col in range(n):
+        print("*",end=' ')
+    print()
+
+
+# output
+
+#    * * * * * 
+#    * * * * * 
+#    * * * * *
+#    * * * * *
+#    * * * * *
