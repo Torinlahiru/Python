a90bec7105ce369bc0e0cd69acf8e8b7fe8e2060
Torin Lahiru <torinlahiru@gmail.com>
2025-05-10 23:25:18 +0530

    diamand created

---
+++ Simple-Programs/Patterns/diamand.py
@@ -0,0 +1,30 @@
+
+
+n = 5
+
+for row in range(n+1):
+    for col in range(n-row):
+        print(" ",end='')
+    for col in range(row):
+        print("*",end=" ")
+    print()
+
+for row in range(n-1,0,-1):
+    for col in range(n-row):
+        print(" ",end='')
+    for col in range(row):
+        print("*",end=" ")
+    print()
+
+
+# Output :
+
+#     * 
+#    * *
+#   * * *
+#  * * * *
+# * * * * *
+#  * * * *
+#   * * *
+#    * *
+#     *
