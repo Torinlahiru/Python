a1026793cbc55bb07e88e2a583e68abb47a54ccd
Torin Lahiru <torinlahiru@gmail.com>
2025-05-12 08:42:20 +0530

    log in function added

--- GUI/login_form/login_form.py
+++ GUI/login_form/login_form.py
@@ -10,6 +10,17 @@ root.configure(bg='#333333')
 
 frame =tkinter.Frame(bg='#333333')
 
+# login function
+
+def login():
+    username = "lahiru"
+    password = "12345"
+
+    if username_entry.get()==username and password_entry.get()==password:
+        print("Successfully logged !")
+    else:
+        print('Invalid user or password ! ')
+
 # Widgets 
 login_label = tkinter.Label(frame,text="Login Form",bg='#333333',foreground='#ffffff',font=("Arial",30))
 username = tkinter.Label(frame,text="Username",bg='#333333',foreground='#ffffff',font=("Arial",16))
@@ -17,7 +28,7 @@ username_entry = tkinter.Entry(frame,font=("Arial",16))
 password = tkinter.Label(frame,text="Password",bg='#333333',foreground='#ffffff',font=("Arial",16))
 password_entry = tkinter.Entry(frame,show="*",font=("Arial",16))
 
-btn_login = tkinter.Button(frame,text="Log In",bg='#d9c725',foreground='#ffffff' ,font=("Arial",16))
+btn_login = tkinter.Button(frame,text="Log In",bg='#d9c725',foreground='#ffffff' ,font=("Arial",16),command=login)
 
 # Place widgets
 
