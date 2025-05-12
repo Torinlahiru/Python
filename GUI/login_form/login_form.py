23fd368e364082b64fb511040df146469e1060ee
Torin Lahiru <torinlahiru@gmail.com>
2025-05-12 08:02:13 +0530

    widgets created

--- GUI/login_form/login_form.py
+++ GUI/login_form/login_form.py
@@ -6,4 +6,13 @@ root.title ="Login Form"
 root.geometry('400x400')
 root.configure(bg='#333333')
 
+login_label = tkinter.Label(root,text="Login Form")
+username = tkinter.Label(root,text="Username")
+username_entry = tkinter.Entry(root)
+password = tkinter.Label(root,text="Password")
+password_entry = tkinter.Entry(root,show="*")
+
+btn_login = tkinter.Button(root,text="Log In")
+
+
 root.mainloop()
