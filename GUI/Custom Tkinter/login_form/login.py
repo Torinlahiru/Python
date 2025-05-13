26e402bbeb98e8840a4220367011af291cf5976a
Torin Lahiru <torinlahiru@gmail.com>
2025-05-13 11:40:44 +0530

    btn added

--- GUI/Custom Tkinter/login_form/login.py
+++ GUI/Custom Tkinter/login_form/login.py
@@ -7,4 +7,10 @@ app = CTk()
 app.title("Login form")
 app.geometry("400x300")
 
+#btn create
+
+btn = CTkButton(master=app,text="Click..",corner_radius=10)
+
+btn.place(relx=0.5,rely=0.5,anchor='center')
+
 app.mainloop()
