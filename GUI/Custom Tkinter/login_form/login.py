b97f57fbd8cff50a7614e290af32a60ddf431a59
Torin Lahiru <torinlahiru@gmail.com>
2025-05-13 11:44:59 +0530

    btn properties updated

--- GUI/Custom Tkinter/login_form/login.py
+++ GUI/Custom Tkinter/login_form/login.py
@@ -9,7 +9,7 @@ app.geometry("400x300")
 
 #btn create
 
-btn = CTkButton(master=app,text="Click..",corner_radius=10)
+btn = CTkButton(master=app,text="Click..",corner_radius=10,fg_color="#0ec3e3",hover_color="#066778")
 
 btn.place(relx=0.5,rely=0.5,anchor='center')
 
