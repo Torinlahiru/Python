2f3d0da186de1f0efeddcbf2bd73996c2b235cd8
Torin Lahiru <torinlahiru@gmail.com>
2025-05-14 23:04:42 +0530

    label added

--- GUI/Custom Tkinter/login_form/login.py
+++ GUI/Custom Tkinter/login_form/login.py
@@ -2,6 +2,11 @@
 
 from customtkinter import *
 
+# import pillow
+
+from PIL import *
+
+
 app = CTk()
 
 app.title("Login form")
@@ -10,10 +15,22 @@ app.geometry("400x300")
 # change light/dark mode
 set_appearance_mode('dark')
 
-#btn create
+# Add a label
+label = CTkLabel(
+    master=app,
+    text="Welcome! Please click",
+    text_color="white",  
+    font=("Arial", 16)   
+)
 
-btn = CTkButton(master=app,text="Click..",corner_radius=10,hover_color="#0ec3e3",fg_color="#066778")
 
+# btn create
+ 
+btn = CTkButton(master=app,text="Click..",corner_radius=10,hover_color="#0ec3e3",fg_color="#066778",border_color="#50f03e",border_width=2)
+
+ 
+
+label.place(relx=0.5, rely=0.3, anchor='center')   
 btn.place(relx=0.5,rely=0.5,anchor='center')
 
 app.mainloop()
