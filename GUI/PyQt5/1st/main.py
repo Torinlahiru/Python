3726ab2b41f1aec6c4817cc8699047ab8ac57e01
Torin Lahiru <torinlahiru@gmail.com>
2025-06-06 22:42:50 +0530

    show window

--- GUI/PyQt5/1st/main.py
+++ GUI/PyQt5/1st/main.py
@@ -1,14 +1,15 @@
 import sys 
-from PyQt5.Qtwidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init()
+        super().__init__a()
 
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
+    window.show()
     
 
 if __name__ == "__main__":
