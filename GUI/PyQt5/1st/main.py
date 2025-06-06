e662bb51e11066774175137bfd3f88a26dfcbb75
Torin Lahiru <torinlahiru@gmail.com>
2025-06-06 22:51:14 +0530

    continued window

--- GUI/PyQt5/1st/main.py
+++ GUI/PyQt5/1st/main.py
@@ -4,12 +4,13 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__a()
+        super().__init__()
 
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
+    sys.exit(app.exec_())
     
 
 if __name__ == "__main__":
