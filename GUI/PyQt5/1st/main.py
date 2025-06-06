a85887c82d83d02b0b9e08d621f9af4c592a69b9
Torin Lahiru <torinlahiru@gmail.com>
2025-06-06 22:39:50 +0530

    main added

--- GUI/PyQt5/1st/main.py
+++ GUI/PyQt5/1st/main.py
@@ -1,2 +1,15 @@
 import sys 
 from PyQt5.Qtwidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init()
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    
+
+if __name__ == "__main__":
+    main()
