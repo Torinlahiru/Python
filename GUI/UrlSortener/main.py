b099454ae164ec5d75cd4318114a70c3c5c01ec9
Torin Lahiru <torinlahiru@gmail.com>
2025-06-14 23:26:57 +0530

    lbl added

--- GUI/UrlSortener/main.py
+++ GUI/UrlSortener/main.py
@@ -7,4 +7,7 @@ from tkinter import messagebox
 root = tk.Tk()
 root.title("URL Shortener")
 root.geometry('400x400')
+
+lbl = tk.Label(root,text="Enter URL ..",font=("Arial",14))
+lbl.pack(pady=10)
 root.mainloop()
